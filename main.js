5475b882453d76aaa7f45ea9fd24b3f301b6822c
Gregory.Johnson <3531op@gmail.com>
2020-10-29 10:20:54 -0400

    Initial commit/Completed assignment

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const onlyOdds = function (nums) {
+  return nums.filter((num) => num % 2 === 1)
+}
 
+const onlyEvens = function (nums) {
+  return nums.filter((num) => num % 2 === 0)
+}
 
+const shortNamesOnly = function (names) {
+  return names.filter((name) => name.length < 7)
+}
+
+const sNames = function (names) {
+  return names.filter((name) => name.startsWith('S'))
+}
+
+const tripleAll = function (nums) {
+  return nums.map((num) => num * 3)
+}
+
+const negativeValues = function (nums) {
+  return nums.map((num) => num > 0 ? num * -1 : num)
+}
+
+const questionings = function (strs) {
+  return strs.map((str) => `${str}?`)
+}
+
+const changeToInitials = function (names) {
+  return names.map((name) => name[0] + name[name.indexOf(' ') + 1])
+}
+
+const doubleEven = function (nums) {
+  return nums.map((num) => num % 2 === 0 ? num * 2 : num)
+}
+
+const titleCaseNames = function (names) {
+  return names.map((name) => name[0].toUpperCase() + name.slice(1).toLowerCase())
+}
+
+const doubleEveryOther = function (nums) {
+  return nums.map((num, index) => index % 2 !== 0 ? num * 2 : num)
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
